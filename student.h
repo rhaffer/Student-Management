9203728bbc0064099796d2c15b7646979d34b66b
rhaffer <44737973+rhaffer@users.noreply.github.com>
2020-03-11 11:39:20 -0600

    Updated student.cpp

--- student.h
+++ student.h
@@ -2,10 +2,17 @@
  * student.h
  *      Author: rhaffer
  */
+#include <iostream>
+#include <string>
+using namespace std;
 
 #ifndef STUDENT_H_
 #define STUDENT_H_
 
+class Student{
+private:
+	string studentID;
+};
 
 
 
