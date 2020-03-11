4a38d453003b0ddd4d8e4f0e2b58f34ff5d61a2c
rhaffer <44737973+rhaffer@users.noreply.github.com>
2020-03-11 11:41:32 -0600

    Add files via upload

---
+++ Haffer_C867_PA/src/student.h
@@ -0,0 +1,20 @@
+/*
+ * student.h
+ *      Author: rhaffer
+ */
+#include <iostream>
+#include <string>
+using namespace std;
+
+#ifndef STUDENT_H_
+#define STUDENT_H_
+
+class Student{
+private:
+	string studentID;
+};
+
+
+
+
+#endif /* STUDENT_H_ */
